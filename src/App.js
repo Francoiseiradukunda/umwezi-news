af675b18cd2aef05e0ea30788b6d89904eddc5f7
iradufrance <Francoiseiradukunda@gmail.com>
2023-11-17 11:06:54 +0000

    updatedd

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './sass/main.scss'
 import Forest from './component/home';
 import Header from './component/header';
 import Importance from './pages/importance';
+import Conclusion from './pages/conclusion';
 function App() {
   return (
     <>
     <Header/>
 <Forest/>
 <Importance/>
+<Conclusion/>
  </> 
  );
 
