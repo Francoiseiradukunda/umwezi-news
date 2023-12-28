7510416843c43da0cac5b9330e299c52328a364d
iradufrance <Francoiseiradukunda@gmail.com>
2023-12-28 08:51:08 +0200

    add ss

--- src/pages/care.jsx
+++ src/pages/care.jsx
@@ -9,18 +9,64 @@ function Care(){
 
            <div className="par1">
             <div className="care1">
-            <h3>1.Reduce, Reuse, Recycle</h3>
+            <h3><u>1.Reduce, Reuse, Recycle</u></h3>
             <p>Minimize waste by using reusable items and recycling materials such as paper, glass, and plastic.
 Opt for products with minimal packaging to reduce overall waste.</p>
             </div>
             
             <div className="care2">
-            <h3>2.Conserve Water</h3>
+            <h3><u>2.Conserve Water</u></h3>
             <p>Fix leaks in faucets and pipes to prevent water wastage.
 Be mindful of water use at home, and consider installing water-saving appliances.</p>
             </div>
 
            </div>
+
+           <div className="par2">
+            <div className="care1">
+            <h3><u>1.Reduce, Reuse, Recycle</u></h3>
+            <p>Minimize waste by using reusable items and recycling materials such as paper, glass, and plastic.
+Opt for products with minimal packaging to reduce overall waste.</p>
+            </div>
+            
+            <div className="care2">
+            <h3><u>2.Conserve Water</u></h3>
+            <p>Fix leaks in faucets and pipes to prevent water wastage.
+Be mindful of water use at home, and consider installing water-saving appliances.</p>
+            </div>
+
+           </div>
+
+           <div className="par3">
+            <div className="care1">
+            <h3><u>1.Reduce, Reuse, Recycle</u></h3>
+            <p>Minimize waste by using reusable items and recycling materials such as paper, glass, and plastic.
+Opt for products with minimal packaging to reduce overall waste.</p>
+            </div>
+            
+            <div className="care2">
+            <h3><u>2.Conserve Water</u></h3>
+            <p>Fix leaks in faucets and pipes to prevent water wastage.
+Be mindful of water use at home, and consider installing water-saving appliances.</p>
+            </div>
+
+           </div>
+
+           <div className="par4">
+            <div className="care1">
+            <h3><u>1.Reduce, Reuse, Recycle</u></h3>
+            <p>Minimize waste by using reusable items and recycling materials such as paper, glass, and plastic.
+Opt for products with minimal packaging to reduce overall waste.</p>
+            </div>
+            
+            <div className="care2">
+            <h3><u>2.Conserve Water</u></h3>
+            <p>Fix leaks in faucets and pipes to prevent water wastage.
+Be mindful of water use at home, and consider installing water-saving appliances.</p>
+            </div>
+
+           </div>
+
         </div>
         </>
     )
